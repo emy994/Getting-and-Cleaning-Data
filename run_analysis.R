50ba778de67cff758d7fb147613b80e0639bac3d
emy994 <57823896+emy994@users.noreply.github.com>
2022-03-26 17:35:14 +0300

    Add files via upload

--- run_analysis.R
+++ run_analysis.R
@@ -1,24 +1,21 @@
-# download the file if not existed
-filename <- "CleanData.zip"
 
-# Checking if archieve already exists.
-if (!file.exists(filename)){
-  fileURL <- "https://d396qusza40orc.cloudfront.net/getdata%2Fprojectfiles%2FUCI%20HAR%20Dataset.zip"
-  download.file(fileURL, filename, method="curl")
-}  
-
-# Checking if folder exists
-if (!file.exists("UCI HAR Dataset")) { 
-  unzip(filename) 
-}
+# get Data
+download.file("https://d396qusza40orc.cloudfront.net/getdata%2Fprojectfiles%2FUCI%20HAR%20Dataset.zip", "CleanData.zip", method="curl")
+ 
 
 # read all data and assign it to tables
 
 features <- read.table("UCI HAR Dataset/features.txt", col.names = c("n","functions"))
 activities <- read.table("UCI HAR Dataset/activity_labels.txt", col.names = c("code", "activity"))
+
+# test data 
+
 subject_test <- read.table("UCI HAR Dataset/test/subject_test.txt", col.names = "subject")
 x_test <- read.table("UCI HAR Dataset/test/X_test.txt", col.names = features$functions)
 y_test <- read.table("UCI HAR Dataset/test/y_test.txt", col.names = "code")
+
+#train data
+
 subject_train <- read.table("UCI HAR Dataset/train/subject_train.txt", col.names = "subject")
 x_train <- read.table("UCI HAR Dataset/train/X_train.txt", col.names = features$functions)
 y_train <- read.table("UCI HAR Dataset/train/y_train.txt", col.names = "code")
@@ -52,10 +49,10 @@ names(TidyData)<-gsub("angle", "Angle", names(TidyData))
 names(TidyData)<-gsub("gravity", "Gravity", names(TidyData))
 
 # 
-FinalData <- TidyData %>%
+TidyData2 <- TidyData %>%
   group_by(subject, activity) %>%
   summarise_all(funs(mean))
-write.table(FinalData, "FinalData.txt", row.name=FALSE)
+write.table(TidyData2, "TidyData2.txt", row.name=FALSE)
 
-str(FinalData)
+str(TidyData2)
 
